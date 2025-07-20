5ecf3587480a6d6edfebc3e71fd5b5d8af76df98
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-07-20 10:51:43 -0400

    Update sitemap.py

--- routes/sitemap.py
+++ routes/sitemap.py
@@ -31,7 +31,6 @@ def sitemap_xml() -> Response:
         {"loc": f"{BASE_URL}/alquiler", "priority": 0.5},
         {"loc": f"{BASE_URL}/limpieza", "priority": 0.5},
         {"loc": f"{BASE_URL}/ventas", "priority": 0.5},
-        {"loc": f"{BASE_URL}/debito", "priority": 0.5},
     ]
     xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
     xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'

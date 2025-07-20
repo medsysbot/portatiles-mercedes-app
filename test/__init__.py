c7b2649d93cc22eedb172e40b5cdb0a83637d0bb
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-07-20 09:46:43 -0400

    Delete test directory

--- test/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-----------------------------------------------------------
-Archivo: test/__init__.py
-Descripción: Inicialización del paquete de tests
-Última modificación: 2025-06-15
-Proyecto: Portátiles Mercedes
-----------------------------------------------------------
-"""

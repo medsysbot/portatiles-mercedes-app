54e36ba19c59b65fecb16452745b04d5c125fe8a
medsysbot <205569692+medsysbot@users.noreply.github.com>
2025-07-26 00:58:46 -0400

    Update guardar_venta.js

--- app_publico/static/js/guardar_venta.js
+++ app_publico/static/js/guardar_venta.js
@@ -39,7 +39,7 @@ form.addEventListener('submit', async (e) => {
 
     if (ok) {
       if (typeof showAlert === 'function') {
-        await showAlert('exito-reporte', 'Formulario enviado correctamente', 2500);
+        await showAlert('reporte-exito', 'Formulario enviado correctamente', 2500);
       }
 
       setTimeout(() => {
@@ -52,12 +52,12 @@ form.addEventListener('submit', async (e) => {
 
     } else {
       if (typeof showAlert === 'function') {
-        await showAlert('error-reporte', resJson.detail || 'Error al enviar el formulario', 2500);
+        await showAlert('reporte-error', resJson.detail || 'Error al enviar el formulario', 2500);
       }
     }
   } catch (_) {
     if (typeof showAlert === 'function') {
-      await showAlert('error-reporte', 'Error al enviar el formulario', 2500);
+      await showAlert('reporte-error', 'Error al enviar el formulario', 2500);
     }
   }
 });
